3ef8268f868ddd97be15087bf5d431fa7cacb73b
Forgottenit <forgottenit@hotmail.com>
2022-10-19 10:59:09 +0000

    Added HTML structure to link html pages and put in placeholders

---
+++ assets/quiz.js
@@ -0,0 +1,10 @@
+//create required variables
+
+
+//get questions from API using fetch depending on game chocie and convert to iterable array
+
+
+//Assign random question and possible answers
+
+
+//Once question answered - check if user got correct answer, if so, increment score and indicate to user the correct answer
